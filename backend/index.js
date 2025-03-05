79bbb5b5bdc0394c8e7ab2b8d0437bd2661f5173
unknown <nicola.picchio@itssmartacademy.it>
2025-03-05 16:59:08 +0100

    added post /createuser with validation through fiscalcode

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,86 @@ var bodyParser = require('body-parser')
 const con = require('./connector.js')
 const port = 3000
 
+
+var jsonParser = bodyParser.json()
+
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+// Enable CORS for all routes
+app.use(cors(corsOptions));
+
+app.post('/createuser', jsonParser, async (req, res) => {
+
+  let requestbody = req.body;
+
+  // user validation
+  try {
+    const validation = await con.execute('select * from users where fiscalcode = ?', [requestbody.fiscalcode]);
+
+    if (validation[0].length < 1) {
+      // user creation
+      const [data] = await con.execute(`insert into users (password, lastname, firstname, phone, email, active, fiscalcode) values (?,?,?,?,?,?,?)`, [requestbody.password, requestbody.lastname, requestbody.firstname, requestbody.phone, requestbody.email, requestbody.active, requestbody.fiscalcode]);
+      res.json(data);
+    }
+    else {
+      res.json({ error: true, errormessage: "FISCALCODE_EXISTS" });
+    }
+
+
+  } catch (err) {
+    console.log(err);
+    res.json({ error: true, errormessage: "GENERIC_ERROR" });
+  }
+});
+
+
+app.get('/getallusers', async (req, res) => {
+  let startvalue = (req.query.startvalue - 1) * req.query.endvalue;
+  let endvalue = (req.query.startvalue * req.query.endvalue) - 1;
+  try {
+    const [data] = await con.execute(`select * from users LIMIT ${startvalue},${endvalue}`);
+    data.forEach((row) => {
+      console.log(`${row.id} = ${row.lastname} ${row.firstname}`);
+    });
+
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+    res.json({ error: true, errormessage: "retrieve users error" });
+  }
+})
+/*
+app.get('/getallusers', async (req, res) => {
+  pagenumber=(req.query.pagenumber - 1) * req.query.pagesize;
+  pagesize=(req.query.pagenumber * req.query.pagesize) - 1;
+  try{
+    const [data] = await con.execute(`select * from users LIMIT ${pagenumber},${pagesize}`);
+    data.forEach( (row) => {
+      console.log(`${row.id} = ${row.lastname} ${row.firstname}`);
+    });
+    
+    res.json(data);
+  } catch(err) {
+    console.log(err);
+    res.json({ error: true, errormessage: "retrieve users error"});
+  }
+  
+})*/
+
+app.post('/Register', jsonParser, (req, res) => {
+  console.log(req.body);
+  res.statusCode = 200;
+  res.send('Register')
+})
+
+
+app.listen(port, () => {
+  console.log(`Its register app listening on port ${port}`)
+})
+
 // inizializzazione connessione
 /*const connection = mysql.createConnection({
     host     : '127.0.0.1', // macchina locale, localhost == 127.0.0.1
@@ -30,58 +110,3 @@ connection.end((err) => {
   // Then sends a quit packet to the MySQL server.
 });
 */
-
-var jsonParser = bodyParser.json()
-
-var corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
-// Enable CORS for all routes
-app.use(cors(corsOptions));
-
-
-app.get('/getallusers', async (req, res) => {
-  let startvalue = (req.query.startvalue - 1) * req.query.endvalue;
-  let endvalue = (req.query.startvalue * req.query.endvalue) - 1;
-    try {
-      const [data] = await con.execute(`select * from users LIMIT ${startvalue},${endvalue}`);
-      data.forEach((row) => {
-        console.log(`${row.id} = ${row.lastname} ${row.firstname}`);
-      });
-
-      res.json(data);
-    } catch (err) {
-      console.log(err);
-      res.json({ error: true, errormessage: "retrieve users error" });
-    }
-})
-/*
-app.get('/getallusers', async (req, res) => {
-  pagenumber=(req.query.pagenumber - 1) * req.query.pagesize;
-  pagesize=(req.query.pagenumber * req.query.pagesize) - 1;
-  try{
-    const [data] = await con.execute(`select * from users LIMIT ${pagenumber},${pagesize}`);
-    data.forEach( (row) => {
-      console.log(`${row.id} = ${row.lastname} ${row.firstname}`);
-    });
-
-    res.json(data);
-  } catch(err) {
-    console.log(err);
-    res.json({ error: true, errormessage: "retrieve users error"});
-  }
-
-})*/
-
-app.post('/Register', jsonParser, (req, res) => {
-  console.log(req.body);
-  res.statusCode = 200;
-  res.send('Register')
-})
-
-
-app.listen(port, () => {
-  console.log(`Its register app listening on port ${port}`)
-})
